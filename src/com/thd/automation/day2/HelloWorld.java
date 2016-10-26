742a1651656d70ae6dc8f33c00c13d1237841f14
Rolando Rivera <rolando.rivera@weather.com>
2016-10-25 21:41:06 -0400

    message

---
+++ src/com/thd/automation/day2/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.thd.automation.day2;
+
+/**
+ * Created by rrolando on 10/25/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        String str = "Hello World!";
+        System.out.println(str);
+    }
+}
---
+++ src/com/thd/automation/day2/PrintGrade.java
@@ -0,0 +1,28 @@
+package com.thd.automation.day2;
+
+/**
+ * Created by rrolando on 10/25/16.
+ */
+public class PrintGrade {
+    public static void main(String[] args){
+        byte test_score;
+
+        test_score = 100;
+
+        if (test_score >= 90){
+            System.out.println("Grade is A.");
+        }
+        else if (test_score >=80 && test_score < 90){
+            System.out.println("Grade is B.");
+        }
+        else if (test_score >=70 && test_score < 80){
+            System.out.println("Grade is C.");
+        }
+        else if (test_score >=60 && test_score < 70){
+            System.out.println("Grade is D.");
+        }
+        else{
+            System.out.println("Grade is F.");
+        }
+    }
+}
